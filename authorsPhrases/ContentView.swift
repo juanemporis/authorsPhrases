20cc945122650d70a4daaf3a2a33f09e62c91207
juanemporis <juanemporis@gmail.com>
2022-06-12 01:24:31 -0500

    Se agrego la struct QuoteData y sus variables para añadir los datos de los autores, también se agrego un @State private var quoteData

--- authorsPhrases/ContentView.swift
+++ authorsPhrases/ContentView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var quoteData : QuoteData?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("authorsPhrases")
+    }
+    private func loadData() {
+        guard let url = URL(string: "https://programming-quotes-api.herokuapp.com/quotes/random") else {
+            return
+        }
     }
 }
+
+//Se agrego un struct para añadir los datos de los autores
+struct QuoteData : Decodable {
+    var _id : String
+    var en : String
+    var author : String
+    var id : String
+  }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
