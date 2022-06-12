6229d85d404ad9b2537628fe3f9c28cd74112510
juanemporis <juanemporis@gmail.com>
2022-06-12 02:44:00 -0500

    Se agrego un buttom, image y un multilineTextAlignment

--- authorsPhrases/ContentView.swift
+++ authorsPhrases/ContentView.swift
@@ -12,7 +12,31 @@ struct ContentView: View {
     @State private var quoteData : QuoteData?
     
     var body: some View {
-        Text("authorsPhrases")
+        HStack {
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                Spacer()
+                
+                Text(quoteData?.en ?? "" )
+                    .font(.title2)
+                Text("- \(quoteData?.author ?? "")")
+                    .font(.title2)
+                    .padding(.top)
+                
+                Spacer()
+                
+                Button(action: loadData) {
+                    Image(systemName: "arrow.clockwise")
+                }
+                .font(.title)
+                .padding(.top)
+                
+            }
+        }
+        multilineTextAlignment(.trailing)
+            .padding()
+            .onAppear(perform: loadData)
             
     }
     private func loadData() {
@@ -24,21 +48,20 @@ struct ContentView: View {
                 return
             }
             if let decodeData = try? JSONDecoder().decode (QuoteData.self, from: data) {
-                DispatchQueue.main.async {
+               DispatchQueue.main.async {
                     self.quoteData = decodeData
                 }
             }
             
-        }
+        }.resume()
     }
 }
 
 //Se agrego un struct para añadir los datos de los autores
 struct QuoteData : Decodable {
-    var _id : String
+    var id : String
     var en : String
     var author : String
-    var id : String
   }
 
 
