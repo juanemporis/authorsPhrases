149fa818e846b48f9147b83eb6eac64fd328968d
levi <levi@williamss-MacBook-Pro.local>
2022-06-11 23:59:11 -0500

    iniciamos el commit

---
+++ authorsPhrases/authorsPhrasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  authorsPhrasesApp.swift
+//  authorsPhrases
+//
+//  Created by levi on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct authorsPhrasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
